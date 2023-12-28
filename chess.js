99ea7aa87047e85ab92f7519a0b8ca0aa48cfcb5
maksimisa <147175028+maksimisa@users.noreply.github.com>
2023-12-28 16:03:38 +0400

    Second commit
    Добавил указание имени при выборе фигуры

--- chess.js
+++ chess.js
@@ -1,6 +1,7 @@
 var map;
 var divSquare = '<div id="s$coord" class="square $color"></div>';
 var divFigure = '<div id="f$coord" class="figure">$figure</div>';
+var divName = '<div id="n$coord" class="name">$name</div>';
 
 $(function (){
     start();
@@ -22,15 +23,16 @@ function setDroppable(){
                 var frCoord = ui.draggable.attr('id').substring(1);
                 var toCoord = this.id.substring(1);
                 moveFigure(frCoord, toCoord);
-                alert('Вы переместили фигуру из ' + frCoord + ' в ' + toCoord)
         }
     });
 }
 
 function moveFigure(frCoord, toCoord){
     figure = map[frCoord];
+    name = map[frCoord];
     showFigureAt(frCoord, '1');
     showFigureAt(toCoord, figure);
+    alert(getNameFigure(name));
 }
 
 function addSquare() {
@@ -57,6 +59,14 @@ function showFigureAt(coord, figure){
     setDraggable();
 }
 
+function showNameAt(coord, name){
+    map[coord] = name;
+    $('#s' + coord).html(divName
+        .replace('$coord', coord)
+        .replace('$name', getNameFigure(name)));
+    setDraggable();
+}
+
 function getChessSimbole(figure){
     switch (figure) {
         case 'K' : return '&#9812;';
@@ -74,6 +84,28 @@ function getChessSimbole(figure){
         default : return'';
     }
 }
+
+function getNameFigure(name){
+    switch (name) {
+        case 'K' : return 'Белый король';
+        case 'Q' : return 'Белая королева';
+        case 'R' : return 'Белая ладья';
+        case 'B' : return 'Белый слон';
+        case 'N' : return 'Белый конь';
+        case 'P' : return 'Белая пешка';
+        case 'k' : return 'Черный король';
+        case 'q' : return 'Черная королева';
+        case 'r' : return 'Черная ладья';
+        case 'b' : return 'Черный слон';
+        case 'n' : return 'Черный конь';
+        case 'p' : return 'Черная пешка';
+        default : return'';
+    }
+}
+
+// Создать метод для получения имен фигур
+// по приципу получения фигур, но возвращаем их имена
+// Сделать отображение имени когда выбираем фигуру
 
 function isBlackSquareAt(coord) {
     return (coord % 8 + Math.floor(coord / 8)) % 2;
